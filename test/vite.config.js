8e983c40c935576a3e287acc62e704490ba7f395
Yarlen <2020030817@upsin.edu.mx>
2025-04-16 16:30:33 -0700

    cambios

--- test/vite.config.js
+++ test/vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   test: {
     //test es La config de vitest
-    environment: "happy-dom",
+    // environment: "happy-dom",
+    environment: "jsdom",
   },
 });
